b9e0bccd18ee71029351d05ae18c307dc0b13be9
Bliskark <fr.tudm05@gmail.com>
2025-04-24 08:57:56 +0200

    verUsuarios con el filtro

--- v2/usuarios/verUsuarios.php
+++ v2/usuarios/verUsuarios.php
@@ -1,23 +1,28 @@
 <?php
-try {
-    $bd = new PDO('mysql:host=PMYSQL168.dns-servicio.com;dbname=9981336_aplimapa;charset=utf8', 'Mapapli', '9R%d5cf62');
+session_start();
 
-    $filtroPermiso = isset($_GET['permiso']) ? $_GET['permiso'] : '';
-    if ($filtroPermiso && $filtroPermiso !== 'todos') {
+try {
+    $bd = new PDO(
+        'mysql:host=PMYSQL168.dns-servicio.com;dbname=9981336_aplimapa;charset=utf8',
+        'Mapapli',
+        '9R%d5cf62'
+    );
+    $filtroPermiso = $_GET['permiso'] ?? 'todos';
+    if ($filtroPermiso !== 'todos') {
         $query = $bd->prepare("SELECT * FROM Usuarios WHERE permiso = ?");
         $query->execute([$filtroPermiso]);
     } else {
         $query = $bd->query("SELECT * FROM Usuarios");
     }
-
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
     exit;
 }
-?>
 
+$isClienteView = $filtroPermiso === 'cliente';
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <title>Ver Usuarios</title>
@@ -26,59 +31,76 @@ try {
 <body>
     <h1>Lista de Usuarios</h1>
 
-    <form method="get" action="">
+    <form method="get">
         <label for="permiso">Filtrar por Permiso:</label>
         <select name="permiso" id="permiso">
-            <option value="todos">Todos</option>
-            <option value="cliente"     <?php if($filtroPermiso=='cliente') echo 'selected';?>>Cliente</option>
-            <option value="recepcion"   <?php if($filtroPermiso=='recepcion') echo 'selected';?>>Recepción</option>
-            <option value="tecnico"     <?php if($filtroPermiso=='tecnico') echo 'selected';?>>Técnico</option>
-            <option value="admin"       <?php if($filtroPermiso=='admin') echo 'selected';?>>Admin</option>
-            <option value="jefeTecnico" <?php if($filtroPermiso=='jefeTecnico') echo 'selected';?>>Jefe Técnico</option>
+            <option value="todos"    <?= $filtroPermiso==='todos'    ? 'selected' : '' ?>>Todos</option>
+            <option value="cliente"  <?= $filtroPermiso==='cliente'  ? 'selected' : '' ?>>Cliente</option>
+            <option value="recepcion"<?= $filtroPermiso==='recepcion'? 'selected' : '' ?>>Recepción</option>
+            <option value="tecnico"  <?= $filtroPermiso==='tecnico'  ? 'selected' : '' ?>>Técnico</option>
+            <option value="admin"    <?= $filtroPermiso==='admin'    ? 'selected' : '' ?>>Admin</option>
+            <option value="jefeTecnico" <?= $filtroPermiso==='jefeTecnico'? 'selected' : '' ?>>Jefe Técnico</option>
         </select>
         <button type="submit">Filtrar</button>
     </form>
     <br>
 
-    <table border="1">
-        <tr>
-            <th>ID</th>
-            <th>Usuario</th>
-            <th>Correo</th>
-            <th>Permiso</th>
-            <th>CP Fiscal</th>
-            <th>Provincia Fiscal</th>
-            <th>Localidad Fiscal</th>
-            <th>Direccion Fiscal</th>
-            <th>CP1</th>
-            <th>Provincia1</th>
-            <th>Localidad1</th>
-            <th>Direccion1</th>
-            <th>CP2</th>
-            <th>Provincia2</th>
-            <th>Localidad2</th>
-            <th></th>Direccion2</th>
-        </tr>
-        <?php while ($row = $query->fetch()): ?>
+    <?php if ($isClienteView): ?>
+        <table border="1">
             <tr>
-                <td><?php echo $row['idUsuarios']; ?></td>
-                <td><?php echo $row['usuario']; ?></td>
-                <td><?php echo $row['correo']; ?></td>
-                <td><?php echo $row['permiso']; ?></td>
-                <td><?php echo $row['cpFiscal']; ?></td>
-                <td><?php echo $row['provinciaFiscal']; ?></td>
-                <td><?php echo $row['localidadFiscal']; ?></td>
-                <td><?php echo $row['direccionFiscal']; ?></td>
-                <td><?php echo $row['cp1']; ?></td>
-                <td><?php echo $row['provincia1']; ?></td>
-                <td><?php echo $row['localidad1']; ?></td>
-                <td><?php echo $row['direccion1']; ?></td>
-                <td><?php echo $row['cp2']; ?></td>
-                <td><?php echo $row['provincia2']; ?></td>
-                <td><?php echo $row['localidad2']; ?></td>
-                <td><?php echo $row['direccion2']; ?></td>
+                <th>ID</th>
+                <th>Usuario</th>
+                <th>Correo</th>
+                <th>Permiso</th>
+                <th>CP Fiscal</th>
+                <th>Provincia Fiscal</th>
+                <th>Localidad Fiscal</th>
+                <th>Dirección Fiscal</th>
+                <th>CP1</th>
+                <th>Provincia1</th>
+                <th>Localidad1</th>
+                <th>Dirección1</th>
+                <th>CP2</th>
+                <th>Provincia2</th>
+                <th>Localidad2</th>
+                <th>Dirección2</th>
             </tr>
-        <?php endwhile; ?>
-    </table>
+            <?php while ($row = $query->fetch()): ?>
+            <tr>
+                <td><?= htmlspecialchars($row['idUsuarios']) ?></td>
+                <td><?= htmlspecialchars($row['usuario']) ?></td>
+                <td><?= htmlspecialchars($row['correo']) ?></td>
+                <td><?= htmlspecialchars($row['permiso']) ?></td>
+                <td><?= htmlspecialchars($row['cpFiscal']) ?></td>
+                <td><?= htmlspecialchars($row['provinciaFiscal']) ?></td>
+                <td><?= htmlspecialchars($row['localidadFiscal']) ?></td>
+                <td><?= htmlspecialchars($row['direccionFiscal']) ?></td>
+                <td><?= htmlspecialchars($row['cp1']) ?></td>
+                <td><?= htmlspecialchars($row['provincia1']) ?></td>
+                <td><?= htmlspecialchars($row['localidad1']) ?></td>
+                <td><?= htmlspecialchars($row['direccion1']) ?></td>
+                <td><?= htmlspecialchars($row['cp2']) ?></td>
+                <td><?= htmlspecialchars($row['provincia2']) ?></td>
+                <td><?= htmlspecialchars($row['localidad2']) ?></td>
+                <td><?= htmlspecialchars($row['direccion2']) ?></td>
+            </tr>
+            <?php endwhile; ?>
+        </table>
+    <?php else: ?>
+        <table border="1">
+            <tr>
+                <th>ID</th>
+                <th>Usuario</th>
+                <th>Correo</th>
+            </tr>
+            <?php while ($row = $query->fetch()): ?>
+            <tr>
+                <td><?= htmlspecialchars($row['idUsuarios']) ?></td>
+                <td><?= htmlspecialchars($row['usuario']) ?></td>
+                <td><?= htmlspecialchars($row['correo']) ?></td>
+            </tr>
+            <?php endwhile; ?>
+        </table>
+    <?php endif; ?>
 </body>
 </html>
